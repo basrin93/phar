7f14de84302a96c335857e4645f5f79c98cdcdfa
rinatbastanov <your-email@example.com>
2025-09-20 11:51:14 +0300

    fiz

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: '/', // Важно! Для корневого деплоя
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  },
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    },
+  },
 })
